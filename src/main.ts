6c0102e25b1d3b37e601d2ed582e71a8f206a4ad
Omochice <mochice.mls.ntl@gmail.com>
2023-07-08 19:35:06 +0900

    :package: replace yargs to cliffy

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 #!/usr/bin/env deno run -A
 
-import yargs from "https://deno.land/x/yargs@v17.7.2-deno/deno.ts";
+import {
+  Command,
+  EnumType,
+} from "https://deno.land/x/cliffy@v1.0.0-rc.2/command/mod.ts";
 import { LuaFactory } from "npm:wasmoon@1.15.0";
 import hljs from "npm:highlight.js@11.8.0";
 import shiki from "npm:shiki@0.14.3";
 
-const highlighter = {
+const supportLanguages = {
   hljs: hljs.listLanguages(),
   shiki: shiki.BUNDLED_LANGUAGES.map((l) => [l.id, l.aliases ?? []]).flat(2),
 } as const satisfies Record<string, string[]>;
+
+const highlight = new EnumType(Object.keys(supportLanguages));
 
 async function fetchPodiumSource(): Promise<string> {
   const url = new URL(
@@ -31,7 +36,6 @@ async function exec(source: string, languages: string[]) {
   );
   try {
     const pod = await lua.doString(code);
-
     const fence = "```";
     for (const language of languages) {
       pod.PodiumBackend.registerSimpleDataParagraph(
@@ -49,8 +53,6 @@ async function exec(source: string, languages: string[]) {
   }
 }
 
-async function getSource(stdin: true): Promise<string>;
-async function getSource(stdin: false, filename: string): Promise<string>;
 async function getSource(stdin: boolean, filename?: string): Promise<string> {
   if (stdin) {
     const decoder = new TextDecoder();
@@ -67,38 +69,32 @@ async function getSource(stdin: boolean, filename?: string): Promise<string> {
 }
 
 if (import.meta.main) {
-  const args = yargs(Deno.args)
-    .scriptName("sample")
-    .strictCommands()
-    .boolean("stdin")
-    .options({
-      in: {
-        nargs: 1,
-        string: true,
-      },
-      out: {
-        nargs: 1,
-        default: undefined,
-        string: true,
-      },
-      highlighter: {
-        choices: Object.keys(highlighter),
-        default: "hljs",
-      },
+  const { options } = await new Command()
+    .name("sample")
+    .type("highlight", highlight)
+    .option("--in <in:string>", "input filepath", {
+      conflicts: ["--stdin"],
+      required: true,
     })
-    .conflicts("stdin", "in")
-    .demandOption("stdin", "in")
-    .help()
-    .parse();
+    .option("--stdin [stdin:boolean]", "use stdin", {
+      conflicts: ["in"],
+      default: false,
+      required: true,
+    })
+    .option("--out <out:string>", "output filename")
+    .option("--highlight <highlight:highlight>", "highlight library name", {
+      default: "hljs",
+    })
+    .parse(Deno.args);
 
   const md = await exec(
-    await getSource(args.stdin, args.in),
+    await getSource(options.stdin, options.in),
     // @ts-ignore: umm...
-    highlighter[args.highlighter] ?? [],
+    supportLanguages[options.highlight] ?? [],
   );
 
-  if (args.out !== undefined) {
-    Deno.writeTextFileSync(args.out, md);
+  if (options.out !== undefined) {
+    Deno.writeTextFileSync(options.out, md);
   } else {
     console.log(md);
   }
