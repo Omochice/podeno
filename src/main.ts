46cb22e995f2b3a6819d543fd5c164cf11714bbc
Omochice <mochice.mls.ntl@gmail.com>
2023-07-20 11:57:59 +0900

    :+1: split main to mod.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
-#!/usr/bin/env deno run -A
-
 import { Command } from "https://deno.land/x/cliffy@v1.0.0-rc.2/command/mod.ts";
 import { command as markdown } from "./subcmd/markdown.ts";
 import { command as vimdoc } from "./subcmd/vimdoc.ts";
 import { ExitCode } from "./exitCode.ts";
 
-async function main(): Promise<ExitCode> {
+export async function main(): Promise<ExitCode> {
   await new Command()
     .name("podeno")
     .command("markdown", markdown)
@@ -14,8 +12,4 @@ async function main(): Promise<ExitCode> {
     .parse(Deno.args);
 
   Deno.exit(1);
-}
-
-if (import.meta.main) {
-  Deno.exit(await main());
 }
