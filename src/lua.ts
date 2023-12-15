a52a6f2ab4286009b4045dac4f24ab7c7f209b63
Omochice <mochice.mls.ntl@gmail.com>
2023-12-15 19:41:20 +0900

    refactor: add comment why use try-catch-finally

--- src/lua.ts
+++ src/lua.ts
@@ -24,6 +24,7 @@ export function execPodium(
       const mergedOption = { highlighter: "hljs", ...option };
       const filetypes = highlightableLanguages
         .get(mergedOption.highlighter) ?? [];
+      // NOTE: use try-catch-finally because of need to use `finally`
       try {
         const pod = lua.doStringSync(code);
         for (const ft of filetypes) {
